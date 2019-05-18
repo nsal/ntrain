82fc65b1d5e4b3297519df8e0aa69dd3b13142cf
Niko <niko.salionov@gmail.com>
2019-05-19 00:20:51 +0100

    link rollback

--- train.py
+++ train.py
@@ -1,7 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 
-link = 'http://ojp.nationalrail.co.uk/service/timesandfares/BFD/BTN/130619/0930/dep'
+link = 'http://ojp.nationalrail.co.uk/service/timesandfares/BFD/BTN/130419/0930/dep'
 
 r = requests.get(link)
 html = BeautifulSoup(r.text, 'html.parser')
