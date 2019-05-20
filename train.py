ecf4dc62be8852e7f618db43e52921609f1dfbca
Niko <niko.salionov@gmail.com>
2019-05-20 22:56:27 +0100

    Cleaned code, added empty class

--- train.py
+++ train.py
@@ -1,10 +1,16 @@
-import requests, re, sys, json
+import requests, re, sys, datetime
 from fake_useragent import UserAgent
 from bs4 import BeautifulSoup
-import datetime
 import pandas as pd
+
+
 price_index = {}
 
+origin = 'BFD'
+destination = 'BTH'
+
+class Ticket:
+    pass
 
 
 def define_holidays():
@@ -20,19 +26,18 @@ def define_holidays():
 
     return calendar_holidays
 
-define_holidays()
 
-def call_for_fares(date):
+def call_for_fares(l_date, r_date, origin, destination):
     """ Call national rails for prices """
-    origin = 'BFD'
-    destination = 'BTH'
-    leaving_date = date
-    return_date = date
+    origin = origin
+    destination = destination
+    leaving_date = l_date
+    return_date = r_date
     leaving_time = '0700'
     return_time = '1830'
-    Passenger = 2
-    rail_card_discount = 0.34
-    prices = []
+    #Passenger = 2
+    #rail_card_discount = 0.34
+
  
 
     link = 'http://ojp.nationalrail.co.uk/service/timesandfares/' + origin + '/' + destination + '/' + leaving_date + '/' + leaving_time + '/dep/' + \
@@ -48,46 +53,33 @@ def call_for_fares(date):
  
     try:
         cheapest_tickets = soup.find(lambda tag: tag.name == 'th' and tag.get('class') == ['fare']).findAll('a')
-
-        #print(date)
-        print(link)
         cheapest_tickets = re.findall(r'£\d{1,3}.\d\d', str(cheapest_tickets))
         cheapest_return = float(cheapest_tickets[0].replace('£', ''))
         cheapest_2singles = float(cheapest_tickets[1].replace('£', ''))
         if cheapest_return < cheapest_2singles:
-            print(f'Cheapest option is Return £{cheapest_return}')
-            price_index[date] = cheapest_return
+            #print(f'Cheapest option is Return £{cheapest_return}')
+            price_index[leaving_date] = cheapest_return
         else:
-            print(f'It is better to buy 2 tickets for £{cheapest_2singles}')
-            price_index[date] = cheapest_2singles
+            #print(f'It is better to buy 2 tickets for £{cheapest_2singles}')
+            price_index[leaving_date] = cheapest_2singles
 
+        # script_blocks = soup.findAll('script')
+        # travel_details_raw = re.findall(r'{"jsonJourneyBreakdown":(.+?)}]}', str(script_blocks))
 
-        # for item in cheapest_tickets:
-        #     item_price = re.search(r'£\d{1,3}.\d\d', item.text.replace('\n', ''))
-        #     item_price = float(item_price[0].replace('£','')) #* Passenger
-        #     prices.append(item_price)
-        #     prices.append(item_price - (item_price * rail_card_discount))
+        # for travel in travel_details_raw:
+        #     try:
+        #         departure_time = re.search(r'"departureTime":"\d\d:\d\d', travel)
+        #         arrival_time = re.search(r'"arrivalTime":"\d\d:\d\d', travel)
+        #         #departure_time = int(travel[161:166].replace(':',''))
+        #         #arrival_time = int(travel[183:188].replace(":",''))
+        #         single_fare_raw = re.search(r'"singleJsonFareBreakdowns":(.+?)}],', travel)
+        #         single_fare = re.search(r'"ticketPrice":\d{1,3}.\d{1,2}', single_fare_raw[0])
+        #         return_fare_raw = re.search(r'"returnJsonFareBreakdowns":(.+?)$', travel)
+        #         return_fare = re.search(r'"ticketPrice":\d{1,3}.\d{1,2}', return_fare_raw[0])
 
-        # print(min(prices))
-        # print('=== \n')
-
-        script_blocks = soup.findAll('script')
-        travel_details_raw = re.findall(r'{"jsonJourneyBreakdown":(.+?)}]}', str(script_blocks))
-
-        for travel in travel_details_raw:
-            try:
-                departure_time = re.search(r'"departureTime":"\d\d:\d\d', travel)
-                arrival_time = re.search(r'"arrivalTime":"\d\d:\d\d', travel)
-                #departure_time = int(travel[161:166].replace(':',''))
-                #arrival_time = int(travel[183:188].replace(":",''))
-                single_fare_raw = re.search(r'"singleJsonFareBreakdowns":(.+?)}],', travel)
-                single_fare = re.search(r'"ticketPrice":\d{1,3}.\d{1,2}', single_fare_raw[0])
-                return_fare_raw = re.search(r'"returnJsonFareBreakdowns":(.+?)$', travel)
-                return_fare = re.search(r'"ticketPrice":\d{1,3}.\d{1,2}', return_fare_raw[0])
-
-                #print(date, departure_time[0], arrival_time[0], single_fare[0], return_fare[0])
-            except Exception:
-                continue
+        #         print(date, departure_time[0], arrival_time[0], single_fare[0], return_fare[0])
+        #     except Exception:
+        #         continue
 
 
         # with open ('2test.html', 'w') as ofile:
@@ -97,21 +89,21 @@ def call_for_fares(date):
     
     return price_index
     
-    
 
 calendar_holidays = define_holidays()
 for day in calendar_holidays:
-    call_for_fares(day)
+    call_for_fares(l_date=day, r_date=day, origin=origin, destination=destination)
 
 values = []
 for item in price_index:
-    values.append(price_index[item])
+    print(item, price_index[item])
+    #values.append(price_index[item])
 
-print(f'Max price: £{max(values)} \nAverage price: £{sum(values) / len(values)} \nMin price: £{min(values)}')
+# print(f'Max price: £{max(values)} \nAverage price: £{sum(values) / len(values)} \nMin price: £{min(values)}')
 
-for item in price_index:
-    if price_index[item] <= min(values) * 1.20:
-        print(item, price_index[item])
+# for item in price_index:
+#     if price_index[item] <= min(values) * 1.20:
+#         print(item, price_index[item])
 
 
 
