a6a96673d81459e562cdfd0ca10dc6d55e4ad761
Niko <niko.salionov@gmail.com>
2019-05-31 17:44:55 +0100

    Reformatted the code accorind to PEP8

--- train.py
+++ train.py
@@ -1,30 +1,46 @@
-import requests, re, sys, datetime, csv
+import requests
+import re
+import sys
+import datetime
+import csv
 from fake_useragent import UserAgent
 from bs4 import BeautifulSoup
 import pandas as pd
 from dataclasses import dataclass
 
+
 @dataclass
 class Ticket:
-    origin      : str
-    destination : str
-    date        : datetime
-    price       : float
+    origin: str
+    destination: str
+    date: int
+    price: float
 
-list_of_tickets = []
 
 origin = 'BFD'
-destinations = ['SAL']#, 'CBG', 'OXF', 'BTH', 'CBW', 'WIN', 'BTN', 'EBN']
-   
+destinations = ['SAL', 'CBG', 'OXF', 'BTH', 'CBW', 'WIN', 'BTN', 'EBN']
+list_of_tickets = []
+website_errors = ['National Rail Enquiries - Oh no! There\'s been a problem!',
+                  '504 - Gateway Timeout']
+
+
 def define_holidays():
     """ Function to find holiadys """
     calendar_holidays = []
     uk_holidays = ['060519', '270519', '260819', '251219', '261219']
-    calendar = pd.date_range(start=datetime.date.today(), periods=60)
-    calendar_business_days = pd.date_range(start=datetime.date.today(), periods=60, freq='B')
+
+    calendar = pd.date_range(
+                             start=datetime.date.today(),
+                             periods=60)
+
+    calendar_business_days = pd.date_range(
+                                            start=datetime.date.today(),
+                                            periods=60,
+                                            freq='B')
 
     for day in calendar:
-        if day not in calendar_business_days or day.strftime('%d%m%y') in uk_holidays :
+        if day not in calendar_business_days or \
+           day.strftime('%d%m%y') in uk_holidays:
             calendar_holidays.append(day.strftime('%d%m%y'))
 
     return calendar_holidays
@@ -38,33 +54,33 @@ def call_for_fares(l_date, r_date, origin, destination):
     return_date = r_date
     leaving_time = '0700'
     return_time = '1830'
-    #Passenger = 2
-    #rail_card_discount = 0.34
+    # Passenger = 2
+    # rail_card_discount = 0.34
 
- 
-
-    link = 'http://ojp.nationalrail.co.uk/service/timesandfares/' + origin + '/' + destination + '/' + leaving_date + '/' + leaving_time + '/dep/' + \
-    return_date + '/' + return_time + '/dep'
+    link = ('http://ojp.nationalrail.co.uk/service/timesandfares/' +
+            origin + '/' + destination + '/' + leaving_date + '/' +
+            leaving_time + '/dep/' + return_date + '/' + return_time + '/dep')
 
     r = requests.get(link, headers={'User-Agent': UserAgent().random})
     soup = BeautifulSoup(r.content, 'html.parser')
 
-
-    if soup.title.text == 'National Rail Enquiries - Oh no! There\'s been a problem!' or  soup.title.text == '504 - Gateway Timeout':
+    if soup.title.text in website_errors:
         print('Website is down')
         sys.exit(0)
- 
+
     try:
-        cheapest_tickets = soup.find(lambda tag: tag.name == 'th' and tag.get('class') == ['fare']).findAll('a')
+        cheapest_tickets = soup.find(lambda tag: tag.name == 'th' and
+                                     tag.get('class') == ['fare']).findAll('a')
+
         cheapest_tickets = re.findall(r'£\d{1,3}.\d\d', str(cheapest_tickets))
         cheapest_return = float(cheapest_tickets[0].replace('£', ''))
         cheapest_2singles = float(cheapest_tickets[1].replace('£', ''))
-        list_of_tickets.append(Ticket(
-            origin = origin,
-            destination = destination,
-            date = datetime.datetime.strptime(leaving_date, '%d%m%y'),
-            price = float(min(cheapest_return, cheapest_2singles))
-            ))
+        list_of_tickets.append(
+            Ticket(
+                   origin=origin,
+                   destination=destination,
+                   date=datetime.datetime.strptime(leaving_date, '%d%m%y'),
+                   price=float(min(cheapest_return, cheapest_2singles))))
 
         # script_blocks = soup.findAll('script')
         # travel_details_raw = re.findall(r'{"jsonJourneyBreakdown":(.+?)}]}', str(script_blocks))
@@ -84,25 +100,32 @@ def call_for_fares(l_date, r_date, origin, destination):
         #     except Exception:
         #         continue
 
-
         # with open ('2test.html', 'w') as ofile:
         #     ofile.write(soup.prettify()) 
     except Exception as e:
         print(e)
-    
+
     return list_of_tickets
-    
+
 
 calendar_holidays = define_holidays()
 for destination in destinations:
     for day in calendar_holidays:
-        call_for_fares(l_date=day, r_date=day, origin=origin, destination=destination)
+        call_for_fares(
+                        l_date=day,
+                        r_date=day,
+                        origin=origin,
+                        destination=destination)
 
-with open ('output.csv', 'w') as csv_file:
+with open('output.csv', 'w') as csv_file:
     for item in list_of_tickets:
         print(item.origin, item.destination, item.date, item.price)
         csv_writer = csv.writer(csv_file, delimiter=",")
-        csv_writer.writerow([item.origin, item.destination, item.date.strftime('%d-%b-%Y'), item.price])
+        csv_writer.writerow([
+                            item.origin,
+                            item.destination,
+                            item.date.strftime('%d-%b-%Y'),
+                            item.price])
 
 
 # cut = soup.findAll(lambda tag: tag.name == 'td' and tag.get('class') == ['fare'])
@@ -117,6 +140,3 @@ with open ('output.csv', 'w') as csv_file:
 #         ticket_price = float(ticket_price[0].split(':')[1]) * 2
 #         ticket_price = round((ticket_price - (ticket_price * 0.34)), 2)
 #         print('Departure time:', departure_time[0].split('"')[3], '\t', 'Arrival time:', arrival_time[0].split('"')[3], '\t', 'Price', ticket_price )
-
-
-
