31309174b284fc0ed9d5c122d6dc283efeb1906b
Niko <niko.salionov@gmail.com>
2019-06-15 10:37:56 +0100

    multi-threading

--- train.py
+++ train.py
@@ -7,6 +7,9 @@ from fake_useragent import UserAgent
 from bs4 import BeautifulSoup
 import pandas as pd
 from dataclasses import dataclass
+import time
+from multiprocessing.dummy import Pool as ThreadPool
+import itertools
 
 
 @dataclass
@@ -18,7 +21,8 @@ class Ticket:
 
 
 origin = 'BFD'
-destinations = ['SAL', 'CBG', 'OXF', 'BTH', 'CBW', 'WIN', 'BTN', 'EBN']
+destinations = ['SAL', 'CBG', 'BTH', 'CBW', 'WIN', 'BTN', 'RYE', 'DVP',
+                'NRW', 'BRI']
 list_of_tickets = []
 website_errors = ['National Rail Enquiries - Oh no! There\'s been a problem!',
                   '504 - Gateway Timeout']
@@ -46,12 +50,12 @@ def define_holidays():
     return calendar_holidays
 
 
-def call_for_fares(l_date, r_date, origin, destination):
+def call_for_fares(l_date, origin, destination):
     """ Call national rails for prices """
     origin = origin
     destination = destination
     leaving_date = l_date
-    return_date = r_date
+    return_date = l_date
     leaving_time = '0700'
     return_time = '1830'
     # Passenger = 2
@@ -83,60 +87,86 @@ def call_for_fares(l_date, r_date, origin, destination):
                    price=float(min(cheapest_return, cheapest_2singles))))
 
         # script_blocks = soup.findAll('script')
-        # travel_details_raw = re.findall(r'{"jsonJourneyBreakdown":(.+?)}]}', str(script_blocks))
+        # travel_details_raw = re.findall(r'{"jsonJourneyBreakdown":(.+?)}]}',
+        #                                 str(script_blocks))
 
         # for travel in travel_details_raw:
         #     try:
-        #         departure_time = re.search(r'"departureTime":"\d\d:\d\d', travel)
-        #         arrival_time = re.search(r'"arrivalTime":"\d\d:\d\d', travel)
+        #         departure_time = re.search(r'"departureTime":"\d\d:\d\d',
+        #                                    travel)
+        #         arrival_time = re.search(r'"arrivalTime":"\d\d:\d\d',
+        #                                  travel)
         #         #departure_time = int(travel[161:166].replace(':',''))
         #         #arrival_time = int(travel[183:188].replace(":",''))
-        #         single_fare_raw = re.search(r'"singleJsonFareBreakdowns":(.+?)}],', travel)
-        #         single_fare = re.search(r'"ticketPrice":\d{1,3}.\d{1,2}', single_fare_raw[0])
-        #         return_fare_raw = re.search(r'"returnJsonFareBreakdowns":(.+?)$', travel)
-        #         return_fare = re.search(r'"ticketPrice":\d{1,3}.\d{1,2}', return_fare_raw[0])
+        #         single_fare_raw = re.search(
+        #                           r'"singleJsonFareBreakdowns":(.+?)}],',
+        #                           travel)
+        #         single_fare = re.search(r'"ticketPrice":\d{1,3}.\d{1,2}',
+        #                                 single_fare_raw[0])
+        #         return_fare_raw = re.search(
+        #                           r'"returnJsonFareBreakdowns":(.+?)$',
+        #                           travel)
+        #         return_fare = re.search(r'"ticketPrice":\d{1,3}.\d{1,2}',
+        #                                 return_fare_raw[0])
 
-        #         print(date, departure_time[0], arrival_time[0], single_fare[0], return_fare[0])
+        #         print(date, departure_time[0], arrival_time[0],
+        #               single_fare[0], return_fare[0])
         #     except Exception:
         #         continue
 
         # with open ('2test.html', 'w') as ofile:
-        #     ofile.write(soup.prettify()) 
+        #     ofile.write(soup.prettify())
     except Exception as e:
         print(e)
 
     return list_of_tickets
 
-
+start = time.time()
 calendar_holidays = define_holidays()
+
+pool = ThreadPool(16)
+
 for destination in destinations:
-    for day in calendar_holidays:
-        call_for_fares(
-                        l_date=day,
-                        r_date=day,
-                        origin=origin,
-                        destination=destination)
+    results = pool.starmap(call_for_fares, zip(calendar_holidays, itertools.repeat(origin), itertools.repeat(destination)))
+pool.close()
+pool.join()
+
+
+#for destination in destinations:
+    
+#     for day in calendar_holidays:
+#         call_for_fares(
+#                         l_date=day,
+#                         r_date=day,
+#                         origin=origin,
+#                         destination=destination)
 
 with open('output.csv', 'w') as csv_file:
+    csv_writer = csv.writer(csv_file, delimiter=",")
+    csv_writer.writerow(['Origin', 'Destination', 'Date', 'Price'])
     for item in list_of_tickets:
         print(item.origin, item.destination, item.date, item.price)
-        csv_writer = csv.writer(csv_file, delimiter=",")
+
         csv_writer.writerow([
                             item.origin,
                             item.destination,
                             item.date.strftime('%d-%b-%Y'),
                             item.price])
+end = time.time()
+print(end - start)
 
-
-# cut = soup.findAll(lambda tag: tag.name == 'td' and tag.get('class') == ['fare'])
+# cut = soup.findAll(lambda tag: tag.name == 'td' and
+#                    tag.get('class') == ['fare'])
 
 
 # for c in cut:
 #     arr = re.findall(r'{"jsonJourneyBreakdown":(.+?)}]}', str(c))
-#     if arr: 
+#     if arr:
 #         departure_time = re.search(r'"departureTime":"\d\d:\d\d', arr[0])
 #         arrival_time = re.search(r'"arrivalTime":"\d\d:\d\d', arr[0])
 #         ticket_price = re.search(r'"ticketPrice":\d{1,3}.\d{1,2}', arr[0])
 #         ticket_price = float(ticket_price[0].split(':')[1]) * 2
 #         ticket_price = round((ticket_price - (ticket_price * 0.34)), 2)
-#         print('Departure time:', departure_time[0].split('"')[3], '\t', 'Arrival time:', arrival_time[0].split('"')[3], '\t', 'Price', ticket_price )
+#         print('Departure time:', departure_time[0].split('"')[3], '\t',
+#               'Arrival time:', arrival_time[0].split('"')[3], '\t', 'Price',
+#                ticket_price )
